86ca2be8eb1a44e9375186a476c27494ca6a0fad
Francisco Miranda <33130113+alvarofranciscomiranda@users.noreply.github.com>
2018-11-12 16:55:55 +0000

    Add files via upload
    Player vs player done

--- Project-1/hawalis.pl
+++ Project-1/hawalis.pl
@@ -1,21 +1,21 @@
 %:- use_module(library(lists)).
 
-%----------------------------------
+%--------------------------------------------------------------------------------
 %Inicial board
-%----------------------------------
+%--------------------------------------------------------------------------------
 initialBoard([
-  [4,7,5,3,1,2,2],
-  [8,2,2,2,2,2,2],
-  [4,2,2,7,2,2,2],
+  [2,2,2,2,2,2,2],
+  [2,2,2,2,2,2,2],
+  [2,2,2,2,2,2,2],
   [2,2,2,2,2,2,2]
 ]).
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Inicial board end
-%----------------------------------
+%---------------------------------------------------------------------------------
 
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Unicode translation
-%----------------------------------
+%---------------------------------------------------------------------------------
 print_cell(0):- put_code(48).
 print_cell(1):- put_code(49). 
 print_cell(2):- put_code(50).
@@ -26,19 +26,19 @@ print_cell(6):- put_code(54).
 print_cell(7):- put_code(55). 
 print_cell(8):- put_code(56).
 print_cell(9):- put_code(57).
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Unicode translation end
-%----------------------------------
+%---------------------------------------------------------------------------------
 
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Board display
-%----------------------------------
+%---------------------------------------------------------------------------------
 drawAll([H | T], Index):-
     drawBoard([H | T], Index).
 
 drawTopBorder:-
     write('                                 '), nl,
-    write('    | 0 | 1 | 2 | 3 | 4 | 5 | 6  '), nl,
+    write('    | 0   1   2   3   4   5   6  '), nl,
     write(' ---+--------------------------- '), nl.
 
 drawBoard([], _).
@@ -46,7 +46,7 @@ drawBoard([H | T], Index) :-
     write('  '), write(Index), write(' |'),
     drawLine(H), 
     nl,
-    write(' -------------------------------'), 
+    write('     ---------------------------'), 
     nl,
     NewIndex is Index+1,
     drawBoard(T, NewIndex).
@@ -57,14 +57,14 @@ drawLine([H | T]) :-
     print_cell(H), 
     write(' |'), 
     drawLine(T).
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Board display end
-%----------------------------------
+%---------------------------------------------------------------------------------
 
 
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Matrix functions
-%----------------------------------
+%---------------------------------------------------------------------------------
 
 getMatrixSeedAt(0, Column, [HeaderLines|_], Seed):-
 	getLineSeedAt(Column, HeaderLines, Seed).
@@ -105,32 +105,31 @@ max_list([H|T], Max0, Max) :-
     Max1 is max(H, Max0),
     max_list(T, Max1, Max).
 
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Matrix functions
-%----------------------------------
+%---------------------------------------------------------------------------------
 
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Game logic
-%----------------------------------
+%---------------------------------------------------------------------------------
+/**
+*adds one seed at Board[Line,Column]
+*/
 incrementOne(Line, Column, Board, NewBoard):-
     getMatrixSeedAt(Line, Column, Board, Seed),
     NewSeed is Seed + 1,
     setMatrixSeedAtWith(Line, Column, NewSeed, Board, NewBoard).
 
-
+/**
+*retrives on Seeds all seeds at Board[Line,Column] putting the position at 0 
+*/
 takeAllSeedsAt(Line, Column, Board, NewBoard, Seeds):-
     getMatrixSeedAt(Line, Column, Board, Seeds),
     setMatrixSeedAtWith(Line, Column, 0, Board, NewBoard).
 
-
-checkMostSeedsLine(0, [HeaderLines|_], MostSeeds):-
-	max_list(HeaderLines, MostSeeds).
-
-checkMostSeedsLine(Line, [_|TailLines], MostSeeds):-
-	Line > 0,
-	NewLine is Line-1,
-	checkMostSeedsLine(NewLine, TailLines, MostSeeds).
-    
+/**
+*retrives in MostSeeds most seeds a player has in his board side
+*/
 checkMostSeedsPlayer1(Board, MostSeeds):-
     checkMostSeedsLine(0, Board, MostSeeds1),
     checkMostSeedsLine(1, Board, MostSeeds2),
@@ -141,29 +140,16 @@ checkMostSeedsPlayer2(Board, MostSeeds):-
     checkMostSeedsLine(3, Board, MostSeeds2),
     MostSeeds is max(MostSeeds1, MostSeeds2).
 
+checkMostSeedsLine(0, [HeaderLines|_], MostSeeds):-
+	max_list(HeaderLines, MostSeeds).
 
-updateCoordsTop(Line, Column, NewLine, NewColumn):-
-    (Line =:= 0; Line =:= 2),
-    Column =:= 0,
-    NewLine is Line + 1,
-    NewColumn is Column.
-
-updateCoordsTop(Line, Column, NewLine, NewColumn):-
-    (Line =:= 0; Line =:= 2),
-    NewLine is Line,
-    NewColumn is Column - 1.
-
-updateCoordsBot(Line,Column, NewLine, NewColumn):-
-    (Line =:= 1; Line =:= 3),
-    Column =:= 6,
-    NewLine is Line - 1,
-    NewColumn is Column.
-
-updateCoordsBot(Line,Column, NewLine, NewColumn):-
-    (Line =:= 1; Line =:= 3),
-    NewLine is Line,
-    NewColumn is Column + 1.
-
+checkMostSeedsLine(Line, [_|TailLines], MostSeeds):-
+	Line > 0,
+	NewLine is Line-1,
+	checkMostSeedsLine(NewLine, TailLines, MostSeeds).
+/**
+*Walk a position in a counter clock way where NewLine and NewColumn are the new coordinates
+*/
 updateCoords(Line,Column, NewLine, NewColumn):-
     (Line =:= 0; Line =:= 2),
     updateCoordsTop(Line,Column, NewLine, NewColumn).
@@ -172,40 +158,262 @@ updateCoords(Line,Column, NewLine, NewColumn):-
     (Line =:= 1; Line =:= 3),
     updateCoordsBot(Line,Column, NewLine, NewColumn).
 
+updateCoordsTop(Line, Column, NewLine, NewColumn):-
+    Column =:= 0,
+    NewLine is Line + 1,
+    NewColumn is Column.
 
-%make a move 
-move(_, _, 0, Board , Board).
-move(Line, Column, Seeds, Board, NewBoard):-
+updateCoordsTop(Line, Column, NewLine, NewColumn):-
+    NewLine is Line,
+    NewColumn is Column - 1.
+
+updateCoordsBot(Line,Column, NewLine, NewColumn):-
+    Column =:= 6,
+    NewLine is Line - 1,
+    NewColumn is Column.
+
+updateCoordsBot(Line,Column, NewLine, NewColumn):-
+    NewLine is Line,
+    NewColumn is Column + 1.
+
+/**
+*Checks if the move where the Board[Line, Column] is played is possible: if yes Boolean is 1; if no Boolean is 0
+*/
+checkPossibleMove(Line, Column, Board, Boolean):-
+    (Line =:= 0; Line =:= 1),
+    checkMovePossiblePlayer1(Line, Column, Board, Boolean).
+
+checkPossibleMove(Line, Column, Board, Boolean):-
+    (Line =:= 2; Line =:= 3),
+    checkMovePossiblePlayer2(Line, Column, Board, Boolean).
+
+checkMovePossiblePlayer1(Line, Column, Board, Boolean):-
+    getMatrixSeedAt(Line, Column, Board, SeedsAt),
+    SeedsAt > 0,
+    checkMostSeedsPlayer1(Board, MostSeeds),
+    MostSeeds =:= 1,
+    updateCoords(Line, Column, NewLine, NewColumn),
+    getMatrixSeedAt(NewLine, NewColumn, Board, SeedsAt2),
+    SeedsAt2 \= 1,
+    Boolean is 1.
+
+checkMovePossiblePlayer1(Line, Column, Board, Boolean):-
+    getMatrixSeedAt(Line, Column, Board, Seeds),
+    Seeds > 1,
+    Boolean is 1.
+
+checkMovePossiblePlayer1(_, _, _, Boolean):-
+    Boolean is 0.
+
+checkMovePossiblePlayer2(Line, Column, Board, Boolean):-
+    getMatrixSeedAt(Line, Column, Board, SeedsAt),
+    SeedsAt > 0,
+    checkMostSeedsPlayer2(Board, MostSeeds),
+    MostSeeds =:= 1,
+    updateCoords(Line, Column, NewLine, NewColumn),
+    getMatrixSeedAt(NewLine, NewColumn, Board, SeedsAt2),
+    SeedsAt2 \= 1,
+    Boolean is 1.
+
+checkMovePossiblePlayer2(Line, Column, Board, Boolean):-
+    getMatrixSeedAt(Line, Column, Board, Seeds),
+    Seeds > 0,
+    checkMostSeedsPlayer2(Board, MostSeeds),
+    MostSeeds > 1,
+    Boolean is 1.
+
+checkMovePossiblePlayer2(_, _, _, Boolean):-
+    Boolean is 0.
+
+/**
+*checks if when a turn ends the player will eat seeds: if yes Boolean is 1; if no Boolean is 0
+*/
+canEat(Line, Column, Board, Boolean):-
+    Line =:= 1,
+    canEatPlayer1(Line, Column, Board, Boolean).
+
+canEat(Line, Column, Board, Boolean):-
+    Line =:= 2,
+    canEatPlayer2(Line, Column, Board, Boolean).
+
+canEat(_, _, _, Boolean):-
+    Boolean is 0.
+
+canEatPlayer1(Line, Column, Board, Boolean):-
+    LineToEat is Line + 1,
+    getMatrixSeedAt(LineToEat, Column, Board, Seed),
+    Seed > 0,
+    Boolean is 1.
+
+canEatPlayer1(_, _, _, Boolean):-
+    Boolean is 0.
+
+canEatPlayer2(Line, Column, Board, Boolean):-
+    LineToEat is Line - 1,
+    getMatrixSeedAt(LineToEat, Column, Board, Seed),
+    Seed > 0,
+    Boolean is 1.
+
+canEatPlayer2(_, _, _, Boolean):-
+    Boolean is 0.
+
+/**
+*eats seeds from the other player
+*/
+eatSeeds(Line, Column, Board, NewBoard):-
+    Line =:= 1,
+    eatSeedsPlayer1(Line, Column, Board, NewBoard).
+
+eatSeeds(Line, Column, Board, NewBoard):-
+    Line =:= 2,
+    eatSeedsPlayer2(Line, Column, Board, NewBoard).
+
+eatSeeds(Line, _, Board, Board):-
+    (Line =:= 0; Line =:= 3),
+    !.
+
+eatSeedsPlayer1(Line, Column, Board, NewBoard):-
+    canEatPlayer1(Line, Column, Board, Boolean),
+    Boolean =:= 1,
+    Line1 is Line + 1,
+    Line2 is Line + 2,
+    setMatrixSeedAtWith(Line1, Column, 0, Board, BoardIntermediate),
+    setMatrixSeedAtWith(Line2, Column, 0, BoardIntermediate, NewBoard).
+
+eatSeedsPlayer1(_, _, Board, Board).
+
+eatSeedsPlayer2(Line, Column, Board, NewBoard):-
+    canEatPlayer2(Line, Column, Board, Boolean),
+    Boolean =:= 1,
+    Line1 is Line - 1,
+    Line2 is Line - 2,
+    setMatrixSeedAtWith(Line1, Column, 0, Board, BoardIntermediate),
+    setMatrixSeedAtWith(Line2, Column, 0, BoardIntermediate, NewBoard).
+
+eatSeedsPlayer2(_, _, Board, Board).
+
+/**
+*checks if either one of the players has won
+*/
+checkWinPlayer1(Board):-
+    checkMostSeedsLine(2, Board, MostSeeds1),
+    checkMostSeedsLine(3, Board, MostSeeds2),
+    MostSeeds is max(MostSeeds1, MostSeeds2),
+    MostSeeds =:= 0,
+    write('Player 1 has Won'),
+    init.
+
+checkWinPlayer1(_).
+    
+checkWinPlayer2(Board):-
+    checkMostSeedsLine(0, Board, MostSeeds1),
+    checkMostSeedsLine(1, Board, MostSeeds2),
+    MostSeeds is max(MostSeeds1, MostSeeds2),
+    MostSeeds =:= 0,
+    write('Player 2 has Won'),
+    init.    
+
+checkWinPlayer2(_).
+
+
+/**
+* make a move
+*/
+movePlayer1(Board, NewBoard):-
+    nl, write('Player 1 turn:'), nl,nl,
+    readPlay(Line, Column),
+    (Line =:= 0; Line =:= 1),
+    checkPossibleMove(Line, Column, Board, Boolean),
+    Boolean =:= 1,
+    takeAllSeedsAt(Line,Column,Board, Board1, Seeds),
+    moveLoop(Line, Column, Seeds, Board1 , NewBoard, 0).
+
+movePlayer1(Board, NewBoard):-
+    clearSreen,
+    write('Can\'t play in those coordinates'), nl,
+    drawTopBorder,
+    drawAll(Board, 0),
+    movePlayer1(Board, NewBoard).
+
+movePlayer2(Board, NewBoard):-
+    nl, write('Player 2 turn:'), nl, nl,
+    readPlay(Line, Column),
+    (Line =:= 2; Line =:= 3),
+    checkPossibleMove(Line, Column, Board, Boolean),
+    Boolean =:= 1,
+    takeAllSeedsAt(Line,Column,Board, Board1, Seeds),
+    moveLoop(Line, Column, Seeds, Board1 , NewBoard, 0).
+
+movePlayer2(Board, NewBoard):-
+    clearSreen,
+    write('Can\'t play in those coordinates'), nl,
+    drawTopBorder,
+    drawAll(Board, 0),
+    movePlayer2(Board, NewBoard).
+    
+moveLoop(Line, Column, 1, Board, FinalBoard, Iteracao):-
+    Iteracao > 0,
+    incrementOne(Line, Column, Board, Board1),
+    eatSeeds(Line, Column, Board1, FinalBoard).
+
+moveLoop(Line, Column, Seeds, Board , X, Iteracao):-
+    Iteracao1 is Iteracao + 1,
+    move(Line, Column, Seeds, FinalLine, FinalColumn, Board , NewBoard),
+    takeAllSeedsAt(FinalLine,FinalColumn, NewBoard, FinalBoard, SeedsAtPos),
+    moveLoop(FinalLine, FinalColumn, SeedsAtPos, FinalBoard, X, Iteracao1), !.
+
+
+move(Line, Column, 0, Line, Column, Board , Board).
+move(Line, Column, Seeds, FinalLine, FinalColumn, Board, NewBoard):-
     Seeds > 0,
     updateCoords(Line,Column,NewLine,NewColumn),
     incrementOne(NewLine,NewColumn,Board, BoardInc),
 
     NewSeeds is Seeds - 1,
-    move(NewLine, NewColumn, NewSeeds, BoardInc, NewBoard).
+    move(NewLine, NewColumn, NewSeeds, FinalLine, FinalColumn, BoardInc, NewBoard).
         
-%----------------------------------
+%---------------------------------------------------------------------------------
 %Game logic end
-%----------------------------------
+%---------------------------------------------------------------------------------
 
-%----------------------------------
+
+%---------------------------------------------------------------------------------
 %Game
-%----------------------------------
-init:-
+%---------------------------------------------------------------------------------
+init:-k
     initialBoard(Tab),
-    cycle(Tab).
+    playerVsPlayer(Tab).
 
-cycle(Board):-
+playerVsPlayer(Board):-
+    clearSreen,
     drawTopBorder,
     drawAll(Board, 0),
     
-    nl,nl,nl,nl,nl,nl,
-    takeAllSeedsAt(0,0,Board, Board1, Seeds),
-    move(0,0,Seeds,Board1, NewBoard),
+    movePlayer1(Board,NewBoard),
+    checkWinPlayer1(NewBoard),
 
+    clearSreen,
     drawTopBorder,
     drawAll(NewBoard, 0),
-    nl.
 
-%----------------------------------
+    movePlayer2(NewBoard, NewBoard2),
+    checkWinPlayer2(NewBoard2),
+
+    playerVsPlayer(NewBoard2).
+
+readPlay(Line, Column) :-
+    write('Insert the coordinates of the cell you want to play'), nl,
+	write('Line: '),
+    read(Line),
+	get_code(_),
+
+    write('Column: '),
+    read(Column),
+	get_code(_).
+
+clearSreen:-
+    nl,nl,nl,nl,nl,nl,nl,nl,nl,nl,nl,nl,nl,nl.
+
+%---------------------------------------------------------------------------------
 %Game end
-%----------------------------------
+%---------------------------------------------------------------------------------
